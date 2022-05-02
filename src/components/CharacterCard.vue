7dd4b71183f035d7adf7af7a5b1c5cf85fa4a497
Elise Gondange <elise@MacBook-Pro-de-Elise.local>
2022-05-02 11:15:02 +0200

    [COMPONENTS] add character cards

---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="card">
+    <img id="characterImg" v-bind:src="imageUrl"> 
+    <p>{{fullName}}</p>
+    <p>{{title}}</p>
+    <p>{{family}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name : 'App',
+    props:{
+      imageUrl: String,
+      fullName : String,
+      title : String,
+      family : String,
+    }
+}
+</script>
+
+<style>
+.card{
+  background-color: peachpuff;
+  padding: 3%;
+  margin: 2%;
+  border-radius: 20px;
+}
+
+#characterImg {
+  max-height: 250px;
+}
+
+</style>
